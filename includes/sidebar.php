e913ed9bfed92b2326beae370ec5f5a4a9b7f092
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-09-25 22:05:36 -0700

    search returns only posts with tag

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,25 +1,9 @@
 <div class="col-md-4">
 
-    <?php
-      if(isset($_POST['submit'])){
-        $search = $_POST['search'];
-        $query = "SELECT * FROM posts WHERE post_tag LIKE '%$search' ";
-        $searchQuery = mysqli_query($connection, $query);
-        if(!$searchQuery){
-          die("QUERY FAILED!" . mysqli_error($connection));
-        }
-        $count = mysqli_num_rows($searchQuery);
-        if($count == 0){
-          echo "<h1>No results</h1>";
-        }
-      }
-    ?>
-
     <!-- Blog Search Well -->
     <div class="well">
      <h4>Blog Search</h4>
-      <form class="" action="" method="post">
-
+      <form class="" action="search.php" method="post">
         <div class="input-group">
             <input name="search" type="text" class="form-control">
             <span class="input-group-btn">
