2b43f6a096105faf6953e2814cf3c9973914046e
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-09-25 20:29:40 -0700

    displayed images related to post

--- index.php
+++ index.php
@@ -25,13 +25,13 @@
                     <small>Secondary Text</small>
                 </h1>
                 <!-- First Blog Post -->
-                <h2> <a href="#"><?php echo $post_title ?></a> </h2>
-                <p class="lead"> by <a href="index.php"><?php echo $post_author ?></a> </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
+                <h2> <a href="#"><?php echo $post_title; ?></a> </h2>
+                <p class="lead"> by <a href="index.php"><?php echo $post_author; ?></a> </p>
+                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
                 <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <img class="img-responsive" src="./admin/images/<?php echo $post_image; ?>" alt="">
                 <hr>
-                <p> <?php echo $post_content ?> </p>
+                <p> <?php echo $post_content; ?> </p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
                 <hr>
               <?php } ?>
